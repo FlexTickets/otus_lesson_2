c591dbb3501194fa12ba57672a5bccdf03471363
FlexTickets <FlexTickets.com@gmail.com>
2023-08-17 21:36:41 +0300

    Add outputs for ssh user and port

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "instance_public_ip" {
   value = yandex_compute_instance.test.network_interface[0].nat_ip_address
 }
 
+output "ssh_user" {
+  value = var.ssh_user
+}
+
+output "ssh_port" {
+  value = var.ssh_port
+}
+
 output "subnet_id" {
   value = data.yandex_vpc_subnet.default.id
 }
